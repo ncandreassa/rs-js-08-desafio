b665a24ad69676d42c7497c9f4219e14f9d3fd47
Nathalia <ncandreassa@hotmail.com>
2023-07-04 20:58:51 +0100

    add dark mode and volume slider

--- script.js
+++ script.js
@@ -8,34 +8,50 @@ const buttonForest = document.querySelector(".forest");
 const buttonRain = document.querySelector(".rain");
 const buttonCoffeeShop = document.querySelector(".coffee-shop");
 const buttonFireplace = document.querySelector(".fireplace");
-const forestSound = new Audio("Floresta.wav");
-const rainSound = new Audio("Chuva.wav");
-const coffeeShopSound = new Audio("Cafeteria.wav");
-const fireplaceSound = new Audio("Lareira.wav");
+const forestSound = new Audio("assets/sounds/Floresta.wav");
+const rainSound = new Audio("assets/sounds/Chuva.wav");
+const coffeeShopSound = new Audio("assets/sounds/Cafeteria.wav");
+const fireplaceSound = new Audio("assets/sounds/Lareira.wav");
+const buttonLightMode = document.querySelector(".light-mode");
+const buttonDarkMode = document.querySelector(".dark-mode");
+const body = document.querySelector("body");
+const forestVolumeSlider = document.querySelector("#forest-volume");
+const rainVolumeSlider = document.querySelector("#rain-volume");
+const coffeeShopVolumeSlider = document.querySelector("#coffee-volume");
+const fireplaceVolumeSlider = document.querySelector("#fireplace-volume");
+
+// Control Variables
 
 let focusTimeOut;
 let isCounting = false;
+let isChangingVolume = false;
+
+// State of each sound button
 
 const sounds = {
   forest: {
     button: buttonForest,
     sound: forestSound,
     playing: false,
+    volume: forestVolumeSlider,
   },
   rain: {
     button: buttonRain,
     sound: rainSound,
     playing: false,
+    volume: rainVolumeSlider,
   },
   coffee: {
     button: buttonCoffeeShop,
     sound: coffeeShopSound,
     playing: false,
+    volume: coffeeShopVolumeSlider,
   },
   fireplace: {
     button: buttonFireplace,
     sound: fireplaceSound,
     playing: false,
+    volume: fireplaceVolumeSlider,
   },
 };
 
@@ -98,8 +114,24 @@ buttonSub.addEventListener("click", function () {
   }
 });
 
+// Change sound volume
+
+function handleVolume(name, value) {
+  isChangingVolume = true;
+  sounds[name].sound.volume = value / 100;
+}
+
+// Play/stop sounds
+
 function toggleSound(name) {
+  if (isChangingVolume) {
+    isChangingVolume = false;
+    return;
+  }
+
   // Check if others are selected and stop playing
+  // Stop music from playing at the same time
+
   for (const key in sounds) {
     if (sounds[key].playing && key != name) {
       sounds[key].sound.pause();
@@ -109,7 +141,8 @@ function toggleSound(name) {
   }
 
   // Check if the one clicked is playing before toggle
-  if (sounds[name].button.classList.contains("selected")) {
+
+  if (sounds[name].playing) {
     sounds[name].sound.pause();
     sounds[name].button.classList.remove("selected");
     sounds[name].playing = false;
@@ -117,8 +150,12 @@ function toggleSound(name) {
     sounds[name].sound.play();
     sounds[name].button.classList.add("selected");
     sounds[name].playing = true;
+    sounds[name].sound.volume = 0.5;
+    sounds[name].volume.value = 50;
   }
 }
+
+// Add play/stop sounds event listeners to buttons
 
 buttonForest.addEventListener("click", function () {
   toggleSound("forest");
@@ -134,4 +171,36 @@ buttonCoffeeShop.addEventListener("click", function () {
 
 buttonFireplace.addEventListener("click", function () {
   toggleSound("fireplace");
+});
+
+// Add toggle between light and dark mode event listeners to buttons
+
+buttonLightMode.addEventListener("click", function () {
+  body.classList.add("dark");
+  buttonLightMode.classList.add("hide");
+  buttonDarkMode.classList.remove("hide");
+});
+
+buttonDarkMode.addEventListener("click", function () {
+  body.classList.remove("dark");
+  buttonLightMode.classList.remove("hide");
+  buttonDarkMode.classList.add("hide");
+});
+
+// Add volume change handlres to buttons
+
+forestVolumeSlider.addEventListener("change", function (event) {
+  handleVolume("forest", event.currentTarget.value);
+});
+
+rainVolumeSlider.addEventListener("change", function (event) {
+  handleVolume("rain", event.currentTarget.value);
+});
+
+coffeeShopVolumeSlider.addEventListener("change", function (event) {
+  handleVolume("coffee", event.currentTarget.value);
+});
+
+fireplaceVolumeSlider.addEventListener("change", function (event) {
+  handleVolume("fireplace", event.currentTarget.value);
 });
